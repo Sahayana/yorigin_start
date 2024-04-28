5a41660948cc514d6991d9193b97a4be676f44f6
GEUMSEONG YANG <admin@adminui-MacBookPro.local>
2024-04-28 15:16:05 +0900

    주석 추가

--- app/entities/collections/shop/shop_collection.py
+++ app/entities/collections/shop/shop_collection.py
@@ -17,6 +17,7 @@ class ShopCollection:
 
     @classmethod
     async def point_intersects(cls, point: GeoJsonPoint) -> list[ShopDocument]:
+        """특정 GeoJSON Object에서 겹치는 데이터 (polygon & point)를 select"""
         return [
             cls._parse(result)
             for result in await cls._collection.find(
@@ -53,6 +54,13 @@ class ShopCollection:
 
     @classmethod
     def _parse(cls, result: dict[Any, Any]) -> ShopDocument:
+        """
+        dict을 쓰지 않고 dto로 굳이 parsing하는 이유
+
+         - dict에는 메서드를 추가할 수 없음 (로직 캡슐화 불가능)
+         - 실행하지 않고 dict 안에 무엇이 있는지 알 수 없음
+         - dict은 정적 타이핑의 도움을 받을 수 없음
+        """
         return ShopDocument(
             _id=result["_id"],
             name=result["name"],
